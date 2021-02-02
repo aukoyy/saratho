8a606034d710c4ae6e1a378b6f1a6b5d8e5620b5
aukoyy <oyvind.auk@gmail.com>
2021-02-02 21:19:33 +0100

    add more nav items

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -8,9 +8,6 @@ import styles from './header.module.css'
 const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
   <div className={styles.root}>
     <div className={styles.wrapper}>
-      <div className={styles.branding}>
-        <Link to='/'>{siteTitle}</Link>
-      </div>
 
       <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
         <Icon symbol='hamburger' />
@@ -19,10 +16,19 @@ const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
       <nav className={cn(styles.nav, showNav && styles.showNav)}>
         <ul>
           <li>
-            <Link to='/archive/'>Archive</Link>
+            <Link to='/'>Main</Link>
           </li>
           <li>
             <Link to='/blog/'>Blog</Link>
+          </li>
+          <li>
+            <Link to='/shop/'>Shop</Link>
+          </li>
+          <li>
+            <Link to='/about/'>About</Link>
+          </li>
+          <li>
+            <Link to='/Colab/'>Colab</Link>
           </li>
         </ul>
       </nav>
