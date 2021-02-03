7834951f50a1e1f693db2dc39b9b5b86d0aebdf4
aukoyy <oyvind.auk@gmail.com>
2021-02-03 12:34:27 +0100

    set up tailwind

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = () => ({
   plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-preset-env')({
       stage: 3,
